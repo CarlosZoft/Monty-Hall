e1f96b27058ddece0c7afc8f86414d7a53fea825
CarlosZoft <carlosoftwar@gmail.com>
2021-12-24 00:26:43 -0300

    feat: initial set up and configuration

---
+++ monty-hall/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Problema de Monty Hall</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
+body {
+  color: #fff;
+  background: linear-gradient(to right, #00c6fb, #005bea);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#app h1 {
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+</style>
